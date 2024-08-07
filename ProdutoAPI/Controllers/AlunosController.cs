346c3f443a57cdf721534a9cbb358a9a0ca97c9e
devrenandias <renandias1612@hotmail.com>
2024-08-06 23:46:02 -0300

    Primeiro Commit

---
+++ ProdutoAPI/Controllers/AlunosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProdutoAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProdutoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        private static List<Aluno> alunos = new List<Aluno>
+        {
+            new Aluno { Id = 10, Nome = "Jeferson Wilians", Email = "jwil@gmail.com", Serie = 9 },
+            new Aluno { Id = 30, Nome = "Wilians Arthur", Email = "art.will@gmail.com", Serie = 5 },
+            new Aluno { Id = 40, Nome = "Lins", Email = "lins@gmail.com", Serie = 1 }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Aluno>> Get()
+        {
+            return Ok(alunos);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Aluno> Get(int id)
+        {
+            var aluno = alunos.FirstOrDefault(p => p.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+            return Ok(aluno);
+        }
+    }
+}
---
+++ ProdutoAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProdutoAPI.Models;
+using System.Collections.Generic;
+
+namespace ProdutosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private static List<Produto> produtos = new List<Produto>
+        {
+            new Produto { Id = 1, Nome = "Produto 1", Preco = 10.0m, Descricao = "Descrição do Produto 1" },
+            new Produto { Id = 2, Nome = "Produto 2", Preco = 20.0m, Descricao = "Descrição do Produto 2" },
+            new Produto { Id = 3, Nome = "Produto 3", Preco = 30.0m, Descricao = "Descrição do Produto 3" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return produtos;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = produtos.Find(p => p.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+    }
+}
